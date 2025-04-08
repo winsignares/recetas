501dea6d0c47ef51f6e5c46eb854981634aca40c
andres <locogb26@gmail.com>
2025-04-08 15:49:14 -0500

    Agregando model UsersModel

---
+++ app/models/UsersModel.py
@@ -0,0 +1,20 @@
+from config.db import app, db, ma
+
+class Users(db.Model):
+    __tablename__ = "tblusers"
+
+    id = db.Column(db.Integer, primary_key = True)
+    fullname = db.Column(db.String(100))
+    email = db.Column(db.String(50))
+
+    def __init__(self, fullname, email):
+        self.fullname = fullname
+        self.email = email
+    
+
+with app.app_context():
+    db.create_all()
+
+class UsersSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'fullname', 'email')

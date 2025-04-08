dbc6050b3951d9ece53a3f9ffc8e99401031157c
andres <locogb26@gmail.com>
2025-04-08 15:57:53 -0500

    Creando relación task - users- category

---
+++ app/models/TaskModel.py
@@ -0,0 +1,23 @@
+from config.db import app, db, ma
+
+class Task(db.Model):
+    __tablename__ = "tblcategory"
+
+    id = db.Column(db.Integer, primary_key = True)
+    nametask = db.Column(db.String(50))
+    id_user_fk = db.Column(db.Integer, db.ForeignKey('tblusers.id'))
+    id_category_fk = db.Column(db.Integer, db.ForeignKey('tblcategory.id'))
+
+    def __init__(self, nametask, id_user_fk, id_category_fk):
+        self.nametask = nametask
+        self.id_user_fk = id_user_fk
+        self.id_category_fk = id_category_fk
+
+
+with app.app_context():
+    db.create_all()
+
+
+class CategorySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'nametask', 'id_user_fk', 'id_category_fk')

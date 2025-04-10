9f88fae441318db6e80b0b5420961e10c642dba2
andres <locogb26@gmail.com>
2025-04-10 15:45:19 -0500

    Agregando endpoints al app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, redirect, render_template
 from config.db import app
 
-
-
-
+from api.UserApi import ruta_user
+from api.CategoryApi import ruta_category
+from api.TaskApi import ruta_task
 
 # Registramos las rutas con blueprint
+app.register_blueprint(ruta_user, url_prefix='api') # localhost:5000/api/user
+app.register_blueprint(ruta_category, url_prefix='api')
+app.register_blueprint(ruta_task, url_prefix='api')
 
 @app.route("/")
 def index():
